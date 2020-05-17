1aeb4ba3ff16e64e201de636a8bf35028e8d8c55
Adrian <adrian_zinko@op.pl>
2020-05-17 07:56:28 +0200

    feat: prepare button interface

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import { default as BemCssModules} from 'bem-css-modules';
 import { default as ButtonStyles} from './Button.module.scss';
 
+type TCalculatorFunction = () => void;
+type TConcatenateFunction = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+
+interface BUttonProps {
+    content: string;
+    isEqual?: boolean;
+    isLighter?: boolean;
+    isMemeory?: boolean;
+    onClick: TCalculatorFunction | TConcatenateFunction;
+}
+
+
 const style = BemCssModules(ButtonStyles);
 
-export const Button: React.FC = (props) => {
+export const Button: React.FC<BUttonProps> = (props) => {
     return (
         <button className={style()}>
+            {props.content}
         </button>
     )
 }
